7d3e10c9af7654d2df2269a78a0a4fd44cff0d22
Eric Brown <Brown.Eric.utmd@gmail.com>
2020-03-24 13:24:11 -0600

    findMemoryType updated
    Function updated to use vulkan.hpp syntax/semantics.

--- vulk_src/TriangleApplication.cpp
+++ vulk_src/TriangleApplication.cpp
@@ -1217,7 +1217,7 @@ TriangleApplication::querySwapChainSupport(vk::PhysicalDevice deviceToQuery) {
 
 uint32_t TriangleApplication::findMemoryType(uint32_t typeFilter, vk::MemoryPropertyFlags properties) {
 	vk::PhysicalDeviceMemoryProperties memProperties;
-	vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+	physicalDevice.getMemoryProperties(&memProperties);
 	for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
 		if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
 			return i;
